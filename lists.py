8adccb3d02d8485d6d9c53fea4f52f03aea1ffa8
Sebastian Gonzalez R <sebastiang1493@gmail.com>
2021-10-23 13:10:19 -0500

    Fixed styling over Lists.py

--- lists.py
+++ lists.py
@@ -17,61 +17,58 @@ from typing import List
 class FixedCommandsList:
 
     def __init__(self):
-        self.fixed_list : List = []
+        self.fixed_list: List = []
 
-    def insert(self,data):
-        i,e = data
-        self.fixed_list.insert(i,e)
-    
+    def insert(self, data):
+        i, e = data
+        self.fixed_list.insert(i, e)
+
     def print(self):
         print(self.fixed_list)
-    
-    def remove(self,e):
+
+    def remove(self, e):
         e = e.pop()
         if e in self.fixed_list:
             self.fixed_list.remove(e)
-    
-    def append(self,e):
+
+    def append(self, e):
         e = e.pop()
         self.fixed_list.append(e)
-    
+
     def sort(self):
         self.fixed_list.sort()
 
     def pop(self):
         self.fixed_list.pop()
-    
+
     def reverse(self):
         self.fixed_list.reverse()
-        
+
 
 def commandSwitcher(commands_list):
     switcher = {
-            'insert' : commands_list.insert,
-            'print' : commands_list.print,
-            'remove' : commands_list.remove,
-            'append' : commands_list.append,
-            'sort' : commands_list.sort,
-            'pop' : commands_list.pop,
-            'reverse' : commands_list.reverse
-        }
+        'insert': commands_list.insert,
+        'print': commands_list.print,
+        'remove': commands_list.remove,
+        'append': commands_list.append,
+        'sort': commands_list.sort,
+        'pop': commands_list.pop,
+        'reverse': commands_list.reverse
+    }
     return switcher
 
-def switch(command,switcher):
+
+def switch(command, switcher):
     return switcher.get(command)
 
-
-    
 
 if __name__ == '__main__':
     N = int(input())
     CommandsList = FixedCommandsList()
     com_switcher = commandSwitcher(CommandsList)
-    for i in range(0,N):
+    for i in range(0, N):
         command_and_parameters = input().split(" ")
         command = command_and_parameters[0]
-        parameters = list(map(int,command_and_parameters[1:]))
-        selection = switch(command,com_switcher)(parameters) if parameters else switch(command,com_switcher)()
-        
-
-    
+        parameters = list(map(int, command_and_parameters[1:]))
+        selection = switch(command, com_switcher)(
+            parameters) if parameters else switch(command, com_switcher)()
