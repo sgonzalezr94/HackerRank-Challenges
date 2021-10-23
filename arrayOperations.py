00172ef529da637fd4eb1a8f376ce3d849f3619c
Sebastian Gonzalez R <sebastiang1493@gmail.com>
2021-10-23 11:14:37 -0500

    Formatted file with pep-8.

--- arrayOperations.py
+++ arrayOperations.py
@@ -6,23 +6,20 @@
 
 from itertools import accumulate
 
-TST_ARRAY = [[1,2,100],[2,5,100],[3,4,100]]
+TST_ARRAY = [[1, 2, 100], [2, 5, 100], [3, 4, 100]]
+
 
 def openTestCase(path):
-    doc = open(path,'r').readlines()
-    newdoc = map( lambda line: list(map(int,line.replace('/n','').split(" "))),doc)
+    doc = open(path, 'r').readlines()
+    newdoc = map(lambda line: list(
+        map(int, line.replace('/n', '').split(" "))), doc)
     return list(newdoc)[1:]
+
 
 def arrayManipulation(n, queries):
     array = [0]*n
     for query in queries:
         array[query[0]-1] += query[2]
-        if query[1]<n:
+        if query[1] < n:
             array[query[1]] -= query[2]
     return max(accumulate(array))
-
-
-hr_test = openTestCase('testCase.txt')
-print(hr_test)
-mau = arrayManipulation(10000000,hr_test)
-print(mau)
